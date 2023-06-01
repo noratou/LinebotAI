c58534f64a20e0366d931c1b65f502f7cf480a9a
noratou <135210731+noratou@users.noreply.github.com>
2023-06-01 21:03:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -41,8 +41,8 @@ if channel_access_token is None:
     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
 
-line_bot_api = LineBotApi(chanel_access_token)
-parser = WebhookParser(chanel_secret)
+line_bot_api = LineBotApi(channel_access_token)
+parser = WebhookParser(channel_secret)
 
 
 @app.route("/callback", methods=['POST'])
